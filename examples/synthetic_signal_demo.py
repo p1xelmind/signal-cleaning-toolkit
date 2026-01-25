1ab2766601117497196ee2bc86a9ecabfc427148
p1xelmind <eduardtrubichkin2406@gmail.com>
2026-01-25 20:48:28 +1000

    commit

--- examples/synthetic_signal_demo.py
+++ examples/synthetic_signal_demo.py
@@ -1,9 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 fs = 250
 duration = 2
 n_samples = int(fs * duration)
+f = 10
 
 t = np.linspace(0 , duration, n_samples) 
 
-signal_alpha = np.sin(2 * np.pi * 10 * t)
+signal_alpha = np.sin(2 * np.pi * f * t)
+
+plt.figure()
+plt.plot(t, signal_alpha)
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.title("Pure 10 Hz Sinusoidal Signal (Alpha Component)")
+plt.show()
