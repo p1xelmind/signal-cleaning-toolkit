85fc9a3b275987f0ecb9d155123c37ba77ef0cee
p1xelmind <eduardtrubichkin2406@gmail.com>
2026-01-26 21:54:52 +1000

    commit

--- examples/synthetic_signal_demo.py
+++ examples/synthetic_signal_demo.py
@@ -4,15 +4,23 @@ import matplotlib.pyplot as plt
 fs = 250
 duration = 2
 n_samples = int(fs * duration)
-f = 10
+f1 = 10
+f2 = 0.5
 
 t = np.linspace(0 , duration, n_samples) 
 
-signal_alpha = np.sin(2 * np.pi * f * t)
+signal_alpha = np.sin(2 * np.pi * f1 * t)
+baseline_drift = np.sin(2 * np.pi * f2 * t)
+
+dirty_signal = signal_alpha + baseline_drift
 
 plt.figure()
-plt.plot(t, signal_alpha)
+plt.plot(t, signal_alpha, label="Alpha (10 Hz)")
+plt.plot(t, baseline_drift, label="Baseline drift (0.5 Hz)")
+plt.plot(t, dirty_signal, label="Dirty signal")
+
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
-plt.title("Pure 10 Hz Sinusoidal Signal (Alpha Component)")
+plt.title("Dirty Signal")
+plt.legend()
 plt.show()
