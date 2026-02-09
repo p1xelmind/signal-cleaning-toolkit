d126c2049a32e803d961c1c0711656c2423d6969
p1xelmind <eduardtrubichkin2406@gmail.com>
2026-02-09 18:39:08 +1000

    commit

--- examples/real_eeg_exploration.py
+++ examples/real_eeg_exploration.py
@@ -1,13 +1,9 @@
 import mne
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 data_path = Path(__file__).parent.parent / "data" / "raw" / "S001R01.edf"
-
 raw = mne.io.read_raw_edf(data_path, preload=True)
-
-print(raw)
-
-raw.info
 
 raw.plot(
     duration=5,
@@ -15,3 +11,23 @@ raw.plot(
     scalings="auto",
     block=True
 )
+
+raw.plot_psd(fmax=60)
+plt.show(block=True)
+
+raw_filt = raw.copy()
+
+raw_filt.filter(
+    l_freq=1.0,
+    h_freq=40.0,
+    picks="eeg",
+    fir_design="firwin"
+)
+
+raw_filt.notch_filter(
+    freqs=50,
+    picks="eeg"
+)
+
+raw_filt.plot_psd(fmax=60)
+plt.show(block=True)
